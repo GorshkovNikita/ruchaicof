d7325832169dd2df3e2b97ef853ba2156d028000
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2015-08-16 21:42:18 +0400

    Fixed names of tables

--- database/migrations/2015_08_03_161138_create_properties_table.php
+++ database/migrations/2015_08_03_161138_create_properties_table.php
@@ -12,7 +12,7 @@ class CreatePropertiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('Properties', function (Blueprint $table) {
+        Schema::create('properties', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('real_name');
