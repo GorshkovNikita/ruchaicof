fc47adba145c68fea7331c2359859330eba2230a
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2015-08-16 21:39:55 +0400

    Fixed names of tables

--- database/seeds/PropertiesTableSeeder.php
+++ database/seeds/PropertiesTableSeeder.php
@@ -11,7 +11,7 @@ class PropertiesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Properties')->insert([
+        DB::table('properties')->insert([
             [
                 'name' => 'Тип',
                 'real_name' => 'type',
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,7 +11,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Users')->insert([
+        DB::table('users')->insert([
             [
                 'name' => 'Петр',
                 'surname' => 'Петров',
